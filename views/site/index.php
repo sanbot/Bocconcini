7cfe20a477d1a6b11554c631727ea085728fa5ea
sanbot <sbotero@cangrix.ingeneo.local>
2016-12-20 14:40:51 -0500

    Cambios iniciales

--- views/site/index.php
+++ views/site/index.php
@@ -2,12 +2,12 @@
 
 /* @var $this yii\web\View */
 
-$this->title = 'My Yii Application';
+$this->title = 'Bocconcini';
 ?>
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Congratulations!</h1>
+        <h1>Bocconcini Sorpresas!</h1>
 
         <p class="lead">You have successfully created your Yii-powered application.</p>
 
